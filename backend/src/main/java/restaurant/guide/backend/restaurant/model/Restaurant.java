442544fe95354aa1fbcbe2d92604b681cfaeab98
greenreeper1 <cedricba.gautier@gmail.com>
2026-06-24 17:13:36 +0200

    refactor: :recycle: add getters and setters to Restaurant

--- backend/src/main/java/restaurant/guide/backend/restaurant/model/Restaurant.java
+++ backend/src/main/java/restaurant/guide/backend/restaurant/model/Restaurant.java
@@ -16,5 +16,39 @@ public class Restaurant {
 
     private String city;
 
-    private String category;    
+    private String category;
+
+    //#region Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }    
+    //#endregion
 }
