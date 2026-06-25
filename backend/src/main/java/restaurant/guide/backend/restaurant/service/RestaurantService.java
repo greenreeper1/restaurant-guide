ebd1d3ba4d6db81cd919077036c4e6cfd90b0397
greenreeper1 <cedricba.gautier@gmail.com>
2026-06-25 14:23:46 +0200

    feat: :sparkles: add restaurant service

---
+++ backend/src/main/java/restaurant/guide/backend/restaurant/service/RestaurantService.java
@@ -0,0 +1,25 @@
+package restaurant.guide.backend.restaurant.service;
+
+import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import restaurant.guide.backend.restaurant.dto.RestaurantResponse;
+import restaurant.guide.backend.restaurant.exceptions.RestaurantNotFoundException;
+import restaurant.guide.backend.restaurant.mapper.RestaurantMapper;
+import restaurant.guide.backend.restaurant.model.Restaurant;
+import restaurant.guide.backend.restaurant.repository.RestaurantRepository;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class RestaurantService {
+    private final RestaurantRepository repository;
+    private final RestaurantMapper mapper;
+
+    public RestaurantResponse getById(Long id){
+        Restaurant restaurant = repository.findById(id).orElseThrow(() -> new RestaurantNotFoundException(id));
+
+        return mapper.toRestaurantResponse(restaurant);
+    }
+}
