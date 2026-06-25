e9c56173a480dacbb300b5efc15558387da40411
greenreeper1 <cedricba.gautier@gmail.com>
2026-06-25 15:04:36 +0200

    feat: :sparkles: add restaurant controller

---
+++ backend/src/main/java/restaurant/guide/backend/restaurant/controller/RestaurantController.java
@@ -0,0 +1,24 @@
+package restaurant.guide.backend.restaurant.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import restaurant.guide.backend.restaurant.dto.RestaurantResponse;
+import restaurant.guide.backend.restaurant.service.RestaurantService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+@RestController
+@RequestMapping("/api/restaurants")
+@RequiredArgsConstructor
+public class RestaurantController {
+    private final RestaurantService service;
+
+    @GetMapping("/{id}")
+    public RestaurantResponse getById(@PathVariable Long id) {
+        return service.getById(id);
+    }
+    
+}
