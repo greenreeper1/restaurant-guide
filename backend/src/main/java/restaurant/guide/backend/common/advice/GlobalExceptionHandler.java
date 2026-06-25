65020bfd256070faa424813224166f1fb7201dba
greenreeper1 <cedricba.gautier@gmail.com>
2026-06-25 15:17:09 +0200

    feat: :sparkles: add global exception handler

---
+++ backend/src/main/java/restaurant/guide/backend/common/advice/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package restaurant.guide.backend.common.advice;
+
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import restaurant.guide.backend.restaurant.exceptions.RestaurantNotFoundException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(RestaurantNotFoundException.class)
+    public ResponseEntity<?> handleRestaurantNotFound(RestaurantNotFoundException ex){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body(Map.of(
+            "error", "BAD_REQUEST",
+            "message", ex.getMessage()
+        ));
+    }
+}
