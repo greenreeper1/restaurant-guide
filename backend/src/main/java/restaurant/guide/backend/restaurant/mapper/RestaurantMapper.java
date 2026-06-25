b47cba2c1f73e1d81b5e6df1c8eec556c95f49c9
greenreeper1 <cedricba.gautier@gmail.com>
2026-06-25 09:39:09 +0200

    fix: :bug: add component annotation to mapper

--- backend/src/main/java/restaurant/guide/backend/restaurant/mapper/RestaurantMapper.java
+++ backend/src/main/java/restaurant/guide/backend/restaurant/mapper/RestaurantMapper.java
@@ -1,8 +1,11 @@
 package restaurant.guide.backend.restaurant.mapper;
 
+import org.springframework.stereotype.Component;
+
 import restaurant.guide.backend.restaurant.dto.RestaurantResponse;
 import restaurant.guide.backend.restaurant.model.Restaurant;
 
+@Component
 public class RestaurantMapper {
     
     public RestaurantResponse toRestaurantResponse(Restaurant restaurant) {
