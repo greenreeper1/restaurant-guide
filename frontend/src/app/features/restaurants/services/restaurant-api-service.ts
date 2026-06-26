fdc39d7cd2ad57ed09def1035084a19656d9ad71
greenreeper1 <cedricba.gautier@gmail.com>
2026-06-26 11:21:03 +0200

    refactor: :pencil2: fix typo in folders

--- frontend/src/app/features/restaurants/services/restaurant-api-service.ts
+++ frontend/src/app/features/restaurants/services/restaurant-api-service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Restaurant } from '../../restaurants/models/restaurants';
+import { Restaurant } from '../models/restaurants';
 
 @Injectable({
   providedIn: 'root',
