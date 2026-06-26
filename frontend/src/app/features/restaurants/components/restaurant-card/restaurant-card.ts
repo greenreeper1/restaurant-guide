5388fe5361520f16e319efa2674db855c218c912
greenreeper1 <cedricba.gautier@gmail.com>
2026-06-26 12:37:22 +0200

    feat: :sparkles: add restaurant card component

---
+++ frontend/src/app/features/restaurants/components/restaurant-card/restaurant-card.ts
@@ -0,0 +1,12 @@
+import { Component, input } from '@angular/core';
+import { Restaurant } from '../../models/restaurants';
+
+@Component({
+  selector: 'app-restaurant-card',
+  imports: [],
+  templateUrl: './restaurant-card.html',
+  styleUrl: './restaurant-card.css',
+})
+export class RestaurantCard {
+  restaurant = input.required<Restaurant>();
+}
