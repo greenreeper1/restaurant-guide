e58f06e04bb9751ee3d2761b60b604e176638b89
greenreeper1 <cedricba.gautier@gmail.com>
2026-06-29 10:17:13 +0200

    feat: :sparkles: add frontend routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', 
+        loadComponent: () => 
+            import('./features/restaurants/pages/restaurant-list-page/restaurant-list-page')
+                .then(m => m.RestaurantListPage)
+    },
+    /*{
+        path: 'restaurants/:id',
+        loadComponent: () => 
+            import('./features/restaurants/pages/restaurant-detail-pages')
+                .then(m => m.RestaurantDetailPage)
+    }*/
+];
