566497c419aad980ffa19a1bf820117676ee6794
greenreeper1 <cedricba.gautier@gmail.com>
2026-06-29 17:18:32 +0200

    refactor: :fire: clean restaurantRepository

--- backend/src/main/java/restaurant/guide/backend/restaurant/repository/RestaurantRepository.java
+++ backend/src/main/java/restaurant/guide/backend/restaurant/repository/RestaurantRepository.java
@@ -10,5 +10,4 @@ import java.util.List;
 @Repository
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
     List<Restaurant> findByCity(String city);
-    List<Restaurant> findAll();
 }
