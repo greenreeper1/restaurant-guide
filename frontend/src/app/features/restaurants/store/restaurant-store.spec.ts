a89d3e8ef4e01ce46d04e96abac67392fe1031ce
greenreeper1 <cedricba.gautier@gmail.com>
2026-07-01 11:14:04 +0200

    test: :white_check_mark: add tests for restaurant store

---
+++ frontend/src/app/features/restaurants/store/restaurant-store.spec.ts
@@ -0,0 +1,112 @@
+import { TestBed } from '@angular/core/testing';
+import { Restaurant } from '../models/restaurants';
+import { RestaurantStore } from './restaurant-store';
+import { RestaurantApiService } from '../services/restaurant-api-service';
+import { of, Subject, throwError } from 'rxjs';
+
+describe('RestaurantStore', () => {
+  let store: RestaurantStore;
+  let api: {
+    getAll: ReturnType<typeof vi.fn>;
+  };
+
+  const restaurants: Restaurant[] = [
+    {
+      id: 1,
+      name: 'Test name 1',
+      city: 'Test city 1',
+      category: 'Test category 1',
+    },
+    {
+      id: 2,
+      name: 'Test name 2',
+      city: 'Test city 2',
+      category: 'Test category 2',
+    },
+  ];
+
+  beforeEach(() => {
+    api = {
+      getAll: vi.fn(),
+    };
+
+    TestBed.configureTestingModule({
+      providers: [
+        RestaurantStore,
+        {
+          provide: RestaurantApiService,
+          useValue: api,
+        },
+      ],
+    });
+    store = TestBed.inject(RestaurantStore);
+  });
+
+  it('should be created', () => {
+    expect(store).toBeTruthy();
+  });
+
+  it('should load restaurants', () => {
+    api.getAll.mockReturnValue(of(restaurants));
+
+    store.load();
+
+    expect(api.getAll).toHaveBeenCalledTimes(1);
+    expect(store.restaurants()).toEqual(restaurants);
+    expect(store.loading()).toBe(false);
+  });
+
+  it('should set loading to true while waiting for the API', () => {
+    const subject = new Subject<Restaurant[]>();
+
+    api.getAll.mockReturnValue(subject);
+
+    store.load();
+
+    expect(store.loading()).toBe(true);
+
+    subject.next(restaurants);
+    subject.complete();
+
+    expect(store.loading()).toBe(false);
+    expect(store.restaurants()).toEqual(restaurants);
+  });
+
+  it('should handle API errors', () => {
+    api.getAll.mockReturnValue(throwError(() => new Error('API error')));
+
+    store.load();
+
+    expect(api.getAll).toHaveBeenCalledTimes(1);
+    expect(store.loading()).toBe(false);
+    expect(store.restaurants()).toEqual([]);
+  });
+
+  it('should replace restaurants on subsequent loads', () => {
+    api.getAll.mockReturnValueOnce(of(restaurants)).mockReturnValueOnce(
+      of([
+        {
+          id: 3,
+          name: 'Test name 3',
+          city: 'Test city 3',
+          category: 'Test category 3',
+        },
+      ]),
+    );
+
+    store.load();
+
+    expect(store.restaurants()).toEqual(restaurants);
+
+    store.load();
+
+    expect(store.restaurants()).toEqual([
+      {
+        id: 3,
+        name: 'Test name 3',
+        city: 'Test city 3',
+        category: 'Test category 3',
+      },
+    ]);
+  });
+});
