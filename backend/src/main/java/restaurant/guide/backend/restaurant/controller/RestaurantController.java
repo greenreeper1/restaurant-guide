5e442997fe01267a315ba9d9725c9d1b4d043093
Cedric GAUTIER <cedric.gautier@aubay.com>
2026-07-20 17:27:46 +0200

    feat: :sparkles: improve post method in controller

--- backend/src/main/java/restaurant/guide/backend/restaurant/controller/RestaurantController.java
+++ backend/src/main/java/restaurant/guide/backend/restaurant/controller/RestaurantController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Slf4j
 @RestController
@@ -41,7 +42,7 @@ public class RestaurantController {
     }
     
     @PostMapping("/register")
-    public RestaurantResponse register(RestaurantResponse restaurantResponse){
+    public RestaurantResponse register(@RequestBody RestaurantResponse restaurantResponse){
         log.info("Adding new restaurant \"{}\" to database", restaurantResponse.name());
         return service.registerRestaurant(restaurantResponse);
     }
