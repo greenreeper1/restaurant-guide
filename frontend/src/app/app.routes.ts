35e6655170517d3417d32d8b5c1b3cb5c0ec9638
Maria30 <saphir3057@gmail.com>
2026-07-29 09:36:52 +0200

    feat: :tada: update frontend

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,13 +1,14 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {
-        path: '', 
-        loadComponent: () => 
-            import('./features/restaurants/pages/restaurant-list-page/restaurant-list-page')
-                .then(m => m.RestaurantListPage)
-    },
-    /*{
+  {
+    path: '',
+    loadComponent: () =>
+      import('./features/restaurants/pages/restaurant-list-page/restaurant-list-page').then(
+        (m) => m.RestaurantListPage,
+      ),
+  },
+  /*{
         path: 'restaurants/:id',
         loadComponent: () => 
             import('./features/restaurants/pages/restaurant-detail-pages')
--- frontend/src/app/features/restaurants/services/restaurant-api-service.ts
+++ frontend/src/app/features/restaurants/services/restaurant-api-service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Restaurant } from '../models/Restaurants';
-import { environment } from '../../../../environments/environment';
+import { environment } from '../../../../environments/environment.dev'; //! A changer selon config
 
 @Injectable({
   providedIn: 'root',
